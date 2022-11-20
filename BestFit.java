bf00503a313350dbfa188304fe84ec9e983c38a1
vinit <khollam.vinit@gmail.com>
2022-11-21 01:39:47 +0530

    added bestfit

--- BestFit.java
+++ BestFit.java
@@ -1,42 +1,50 @@
 import java.util.*;
 
-public class FirstFit {
+public class BestFit {
     public static HashMap<Integer, Integer> MemoryBlock = new HashMap<Integer, Integer>();
     public static HashMap<Integer, Integer> ProcessID = new HashMap<Integer, Integer>();
     public static HashMap<Integer, Integer> ProcessBlock = new HashMap<Integer, Integer>();
     public static HashMap<Integer, Integer> MemoryID = new HashMap<Integer, Integer>();
 
     public static void SearchHole(int pid, int pspace) {
-        // iterate over memory block and assign memory block where space is more than process size 
+        // iterate over memory block and assign memory block where space is more than
+        // process size
+
+        int minFragment = Integer.MAX_VALUE;
+        int minFragmentID = -1;
 
         for (Map.Entry<Integer, Integer> entry : MemoryBlock.entrySet()) {
             int mid = entry.getKey();
             int msize = entry.getValue();
 
             if (pspace < msize && MemoryID.get(mid) == -1) {
-                MemoryID.put(mid, pid);
-                ProcessID.put(pid, mid);
-                return;
-            }
+                if (minFragment > msize - pspace) {
+                    minFragment = msize - pspace;
+                    minFragmentID = mid;
+                }
+            } 
 
         }
+
+        ProcessID.put(pid, minFragmentID);
+        MemoryID.put(minFragmentID, pid);
+
     }
 
     public static void print() {
         for (Map.Entry<Integer, Integer> entry : ProcessID.entrySet()) {
-            String str = entry.getValue() == -1 ? "No Space Allocated" : entry.getValue().toString();
-            System.out.println(entry.getKey() + " - " + str);
+             int pid = entry.getKey();
+             int block = entry.getValue();
+             System.out.println("Process ID - "+pid+" Process Size - "+ProcessBlock.get(pid)+" Block Allocated - "+ block );
         }
     }
 
-    public static void FirstFitAlloc() {
+    public static void BestFitAlloc() {
         // iterate over all the process and find the hole in memory block
         for (Map.Entry<Integer, Integer> process : ProcessBlock.entrySet()) {
             SearchHole(process.getKey(), process.getValue());
         }
     }
-     
-
 
     public static void main(String[] args) {
 
@@ -53,7 +61,7 @@ public class FirstFit {
         MemoryBlock.put(1, 100);
         MemoryBlock.put(2, 200);
         MemoryBlock.put(3, 300);
-        MemoryBlock.put(4, 300);
+        MemoryBlock.put(4, 250);
         MemoryBlock.put(5, 600);
 
         MemoryID.put(1, -1);
@@ -63,7 +71,7 @@ public class FirstFit {
         MemoryID.put(5, -1);
 
         // iterate over process block and search for empty space
-        FirstFitAlloc();
+        BestFitAlloc();
 
         print();
     }
